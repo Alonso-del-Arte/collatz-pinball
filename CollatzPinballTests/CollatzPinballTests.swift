57dd744d4d938dea142d707d6076f7fa5c287c1d
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-01 23:35:32 -0400

    Initial commit

---
+++ CollatzPinballTests/CollatzPinballTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollatzPinballTests.swift
+//  CollatzPinballTests
+//
+//  Created by Alonso del Arte on 5/1/25.
+//
+
+import Testing
+@testable import CollatzPinball
+
+struct CollatzPinballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
